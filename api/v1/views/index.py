5c1c9dffa6690a3f0307aa4e0a82620b22d0a4a1
NgigiN <ngigi.nyongo@gmail.com>
2023-07-31 21:06:26 +0300

    fixing commenting issues

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+# Path: api/v1/views/index.py
+# Compare this snippet from models/engine/file_storage.py:
+
 
 from api.v1.views import app_views
 from flask import jsonify, request, abort
