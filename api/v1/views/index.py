0ea1d592254f659bae96a7e5c4e1ac31acedd36e
NgigiN <ngigi.nyongo@gmail.com>
2023-07-31 21:10:52 +0300

    added feature to get the stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,3 +12,14 @@ from models import storage_t
 def status():
     """" Returns a JSON: "status": "OK" """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """" Returns a JSON: "status": "OK" """
+    return jsonify({"amenities": storage_t.count("Amenity"),
+                    "cities": storage_t.count("City"),
+                    "places": storage_t.count("Place"),
+                    "reviews": storage_t.count("Review"),
+                    "states": storage_t.count("State"),
+                    "users": storage_t.count("User")})
