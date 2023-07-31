32cc8efd05e725a2b447dfb91959384453173092
NgigiN <ngigi.nyongo@gmail.com>
2023-07-31 21:39:28 +0300

    adding cities

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,5 +5,8 @@
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+'''The blueprint for the Airbnb API.'''
 
+from api.v1.views.cities import *
 from api.v1.views.index import *
+from api.v1.views.states import *
